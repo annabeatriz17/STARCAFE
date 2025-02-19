594a644f806c39e6f2424aaca1d07ec3892e36eb
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-02-19 13:58:19 -0300

    Update Order.js

---
+++ src/models/Order.js
@@ -0,0 +1,11 @@
+const { v4: uuid4 } = require('uuid');
+
+class Order {
+    constructor(id, item, status) {
+        this.id = uuid4();
+        this.item = item;
+        this.status = status;
+    }
+}
+
+module.exports = Order;
