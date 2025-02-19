11cbaae9a61f805108e4b4a1377a9d4489395b40
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-02-19 15:45:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const menuRoutes = require("./src/routes/menuRoutes");
 
 const app = express();
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", menuRoutes);
 
-const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) =>{
+    res.send("AMO BACK-END!!")
+});
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
