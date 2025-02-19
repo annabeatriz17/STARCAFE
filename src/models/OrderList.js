ccf1fda44c2c373d7b829c55cef113a8ec1958d8
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-02-19 14:02:42 -0300

    Update OrderList.js

---
+++ src/models/OrderList.js
@@ -0,0 +1,23 @@
+class OrderList {
+    constructor() {
+        this.order = [];
+    }
+    addOrder(order) {
+        this.order.push(order);
+    }
+    getAllOrder() {
+        return this.order;
+    }
+    getOrderById(id) {
+        const order = this.order.find(order => order.id == id);
+        if (!order) {
+            throw new Error('Pedido não encontrado');
+        }
+        return order;
+    }
+    deleteOrder(id) {
+        this.order = this.order.filter(order => order.id != id);
+    }
+}
+
+module.exports = OrderList;
